694ad9f2cf6739f7f2c6098a5d2bccaac7c1360d
MariosGen <mariosgen@athtech.gr>
2021-05-23 14:58:35 +0300

    last minute changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         List<String> words = Arrays.asList("Anna", "Bob", "level", "tenet", "Race Car", "Top spot", "Never odd or even", "barber", "avalanche", "a   a", "civic", "madam", "Sagas", "wow", "Red rum sir is murder", "Eva can I see bees in a cave");
         for (String original : words) {
-            String replacer = original.toLowerCase(Locale.ROOT).replaceAll("\\s|", "");
+            String replacer = original.toLowerCase(Locale.ROOT).replaceAll("\\s", "");
             StringBuilder reverse = new StringBuilder();
             int length = replacer.length();
             for (int i = length - 1; i >= 0; i--)
